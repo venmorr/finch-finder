3eca3ae4bedcd92b47d945155881c523caf33661
venmorr <stevenmorrrca@email.com>
2023-11-01 14:24:02 -0400

    add admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-class Finch:
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
+from .models import Finch
 
 # Define the home view
 def home(request):
@@ -16,13 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def finch_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
-
-
-
-finches = [
-  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
-  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
