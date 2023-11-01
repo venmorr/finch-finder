956baa67675f7e519f117f599d1c34a2f18b701c
venmorr <stevenmorrrca@email.com>
2023-11-01 13:32:37 -0400

    add finch index

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+class Finch:
+  def __init__(self, name, type, description, age):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.age = age
 
 # Define the home view
 def home(request):
@@ -7,3 +14,15 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def finch_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
