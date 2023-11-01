c29f476f7710c88f60e7e96ddc1cf167027dd888
venmorr <stevenmorrrca@email.com>
2023-11-01 12:02:07 -0400

    set up home and about

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-# Add the include function to the import
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # In this case '' represents the root route
-    path('', include('main_app.urls')),
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  # path('finches/', views.finch_index, name='finch-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
+def about(request):
+  return render(request, 'about.html')
