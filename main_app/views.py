6f89a971523a3c6f0d2b59cdde5210031aea4d78
venmorr <stevenmorrrca@email.com>
2023-11-01 14:59:31 -0400

    add finch details

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,9 @@ def about(request):
 def finch_index(request):
   finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
+
